2e351c564c1ba41e3944a61277bc39024118dad6
sharareh <sh.heidarzadeh@yahoo.com>
2018-11-27 12:49:56 +0330

    Update app-routing.module.ts
    fix bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SymbolListComponent } from './symbol-list/symbol-list.component';
 import { SymbolDetailesComponent } from './symbol-detailes/symbol-detailes.component';
-import { SymbolCompaireComponent } from './symbol-compaire/symbol-compaire.component';
+// import { SymbolCompaireComponent } from './symbol-compaire/symbol-compaire.component';
 const routes: Routes = [
    { path: '', component: SymbolListComponent },
   { path: 'details/:id', component: SymbolDetailesComponent },
